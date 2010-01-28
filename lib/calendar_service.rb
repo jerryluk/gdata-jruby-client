867e4dae1b2c6f590317e1aa736d9879fa2daa59
Jerry Luk <jerry@Eva.local>
2010-01-27 21:09:31 -0800

    Added methods to specify what feed and entry for calendar service to get

--- lib/calendar_service.rb
+++ lib/calendar_service.rb
@@ -9,11 +9,27 @@ class Java::ComGoogleGdataClientCalendar::CalendarService
   OWN_CALENDAR_URL = 'http://www.google.com/calendar/feeds/default/owncalendars/full'
   DEFAULT_CALENDAR_URI = 'http://www.google.com/calendar/feeds/default/private/full'
   
-  def find_feed(options)
-    super(options.merge({:class => GData::CalendarFeed}))
+  def find_feed(options, klass = GData::CalendarFeed)
+    super(options.merge({:class => klass}))
   end
   
-  def find_entry(options)
-    super(options.merge({:class => GData::CalendarEntry}))
+  def find_calendars_feed(options)
+    find_feed(options, GData::CalendarFeed)
+  end
+  
+  def find_events_feed(options)
+    find_feed(options, GData::CalendarEventFeed)
+  end
+  
+  def find_entry(options, klass = GData::CalendarEntry)
+    super(options.merge({:class => klass}))
+  end
+  
+  def find_calendar_entry(options)
+    find_entry(options, GData::CalendarEntry)
+  end
+  
+  def find_event_entry(options)
+    find_entry(options, GData::CalendarEventEntry)
   end
 end
